51d7948ee434b647855423a3bd37602fd6c06de1
Dice_kl17 <Dice_kl17@student.itstep.org>
2021-04-16 01:42:07 +0300

    контрольное сохранение

--- Game_Shop_V_2/ViewModel/ModelView.cs
+++ Game_Shop_V_2/ViewModel/ModelView.cs
@@ -15,13 +15,15 @@ namespace Game_Shop_V_2.ViewModel
     {
         DB_Game sourse;
         public  ObservableCollection<Game> Games { get; set; }
+        public  ObservableCollection<Model.Base.Style> Styles { get; set; }
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string prop = "")
            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
         public ModelView()
         {
             sourse = new DB_Game();
-            Update_Games();        
+            Update_Games();
+           
         }
      
 
@@ -30,6 +32,14 @@ namespace Game_Shop_V_2.ViewModel
         {
             get { return curent_game; }
             set { curent_game = value; OnPropertyChanged("Curent_game"); }
+        }
+
+
+        private Model.Base.Style curent_style;
+        public Model.Base.Style Curent_style
+        {
+            get { return curent_style; }
+            set { curent_style = value; OnPropertyChanged("Curent_style"); }
         }
 
         internal  void Save() // сохранение изминений
@@ -55,6 +65,11 @@ namespace Game_Shop_V_2.ViewModel
                 Games = new ObservableCollection<Game>();
             Games.Clear();
             sourse.Games.ToList().ForEach(i => Games.Add(i));
+            if (Styles == null)
+                Styles = new ObservableCollection<Model.Base.Style>();
+            Styles.Clear();
+            sourse.Styles.ToList().ForEach(i => Styles.Add(i));
+           /* Curent_style = sourse.Styles.ToList().Find()*/
         }
         internal  void Dell()
         {
