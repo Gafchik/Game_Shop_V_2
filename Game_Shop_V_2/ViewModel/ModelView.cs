bbe2b58d8908ca0237e6f09872056ecb2de052d2
student <student@COMP-209>
2021-04-18 12:30:12 +0300

    Классная раббота

--- Game_Shop_V_2/ViewModel/ModelView.cs
+++ Game_Shop_V_2/ViewModel/ModelView.cs
@@ -1,5 +1,5 @@
-﻿using Game_Shop_V_2.Model.Base;
-using System;
+﻿using System;
+using Game_Shop_V_2.Model.Base;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -8,22 +8,23 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Game_Shop_V_2.View.ViewModel;
 
 namespace Game_Shop_V_2.ViewModel
 {
     public class ModelView : INotifyPropertyChanged
     {
-       public DB_Game sourse;
-        public  ObservableCollection<Game> Games { get; set; }
+        public DB_Game sourse;
+        public ObservableCollection<Game> Games { get; set; }
 
 
-        public  ObservableCollection<Studio> Studios { get; set; }
+        public ObservableCollection<Studio> Studios { get; set; }
 
 
-        public  ObservableCollection<Model.Base.Style> Styles { get; set; }
+        public ObservableCollection<Model.Base.Style> Styles { get; set; }
 
 
-        public  ObservableCollection<Mod_Game> Mod_s { get; set; }
+        public ObservableCollection<Mod_Game> Mod_s { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string prop = "")
@@ -42,6 +43,38 @@ namespace Game_Shop_V_2.ViewModel
 
         }
 
+        private RelayCommand serch_comand;
+        public RelayCommand Serch_comand
+        {
+            get
+            {
+                return serch_comand ?? (serch_comand = new RelayCommand(act =>
+                {
+                    if (Games != null)
+                        Games = new ObservableCollection<Game>(Find(i => i.Game_Name.ToLower().Contains(Serch_srt.ToLower())));
+                    OnPropertyChanged("Games");
+                }));
+
+
+            }
+        }
+
+       IEnumerable<Game> Find(Func<Game, bool> filter) => sourse.Games.ToList().Where(filter).ToList();
+       
+
+
+        private string serch_srt;
+
+        public string Serch_srt
+        {
+            get { return serch_srt; }
+            set { serch_srt = value; OnPropertyChanged("Serch_srt"); Games = new ObservableCollection<Game>(Find(i => i.Game_Name.ToLower().Contains(Serch_srt.ToLower())));
+                OnPropertyChanged("Games"); 
+            }
+        }
+
+
+
 
         private Game curent_game;
         public Game Curent_game
@@ -49,8 +82,8 @@ namespace Game_Shop_V_2.ViewModel
             get { return curent_game; }
             set { curent_game = value; OnPropertyChanged("Curent_game"); }
         }
-        
-        
+
+
         private Mod_Game curent_mod;
         public Mod_Game Curent_mod
         {
@@ -82,7 +115,7 @@ namespace Game_Shop_V_2.ViewModel
             set { curent_style = value; OnPropertyChanged("Curent_style"); }
         }
 
-        internal  void Save() // сохранение изминений
+        internal void Save() // сохранение изминений
         {
             if (MessageBox.Show("Сохранить изминения?", "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
                 return;
@@ -96,7 +129,7 @@ namespace Game_Shop_V_2.ViewModel
                 else
                     MessageBox.Show("Что-то пошло нет так", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-           
+
         }
 
         public void Update_Games() // обновление  списка
@@ -117,9 +150,9 @@ namespace Game_Shop_V_2.ViewModel
                 Mod_s = new ObservableCollection<Mod_Game>();
             Mod_s.Clear();
             sourse.Mod_Game.ToList().ForEach(i => Mod_s.Add(i));
-         
+
         }
-        internal  void Dell()
+        internal void Dell()
         {
             if (MessageBox.Show("Удалить игру?", "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
                 return;
