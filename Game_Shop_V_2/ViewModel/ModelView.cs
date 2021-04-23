179b07826303beafde5f131f8b642a55d73fcd6f
Dice_kl17 <Dice_kl17@student.itstep.org>
2021-04-23 20:01:59 +0300

    ready

--- Game_Shop_V_2/ViewModel/ModelView.cs
+++ Game_Shop_V_2/ViewModel/ModelView.cs
@@ -88,34 +88,7 @@ namespace Game_Shop_V_2.ViewModel
             get { return curent_game; }
             set { curent_game = value; OnPropertyChanged("Curent_game"); }
         }
-        #endregion
-
-        #region curent_mod
-        private Mod_Game curent_mod;
-        public Mod_Game Curent_mod
-        {
-            get { return curent_mod; }
-            set { curent_mod = value; OnPropertyChanged("Curent_game"); }
-        }
-        #endregion
-
-        #region curent_studio
-        private Studio curent_studio;
-        public Studio Curent_studio
-        {
-            get { return curent_studio; }
-            set { curent_studio = value; OnPropertyChanged("Curent_game"); }
-        }
-        #endregion
-
-        #region curent_style
-        private Model.Base.Style curent_style;
-        public Model.Base.Style Curent_style
-        {
-            get { return curent_style; }
-            set { curent_style = value; OnPropertyChanged("Curent_style"); }
-        }
-        #endregion
+        #endregion         
 
         #region Save
 
@@ -180,113 +153,81 @@ namespace Game_Shop_V_2.ViewModel
             }
         }
 
-       /* internal void Save() // сохранение изминений
-        {
-            if (MessageBox.Show("Сохранить изминения?", "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
-                return;
-            else
-            {
-                if (Games.ToList().Exists(i => i.Id == Curent_game.Id))
-                {
-                    using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DB_Game"].ConnectionString))
-                    {
-                        db.Open();
-                        using (var transaction = db.BeginTransaction())
-                        {
-                            try
-                            {
+        #endregion
 
-                                db.Execute("UPDATE [Game] SET Game_Name = @Game_Name," + "Game_Studio_id = @Game_Studio_id," +
-                                            "Game_Style_id = @Game_Style_id," + "Game_Year_Releas = @Game_Year_Releas," +
-                                            "Game_Mod_id = @Game_Mod_id," + "Game_Count_Sell = @Game_Count_Sell " +
-                                              "WHERE Id = @Id",
-                                    new
-                                    {
-                                        Id = Curent_game.Id,
-                                        Game_Name = Curent_game.Game_Name,
-                                        Game_Studio_id = Curent_game.Studio.Id,
-                                        Game_Style_id = Curent_game.Style.Id,
-                                        Game_Year_Releas = Curent_game.Game_Year_Releas,
-                                        Game_Mod_id = Curent_game.Mod_Game.Id,
-                                        Game_Count_Sell = Curent_game.Game_Count_Sell
-                                    }, transaction);
-                                transaction.Commit();
-                            }
-                            catch (System.Exception ex)
+        #region Dell
+        private RelayCommand dell;
+        public RelayCommand Dell
+        {
+            get
+            {
+                return dell ?? (dell = new RelayCommand(act =>
+                {
+                    if (MessageBox.Show("Удалить игру?", "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
+                        return;
+                    else
+                    {
+                        if (Games.ToList().Exists(i => i.Id == Curent_game.Id))
+                        {
+                            using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DB_Game"].ConnectionString))
                             {
-                                transaction.Rollback();
-                                throw ex;
+                                db.Open();
+                                using (var transaction = db.BeginTransaction())
+                                {
+                                    try
+                                    {
+                                        db.Execute(" DELETE FROM [Game] WHERE Id = @Id",
+                                            new { Id = Curent_game.Id }, transaction);
+                                        transaction.Commit();
+                                    }
+                                    catch (System.Exception ex)
+                                    {
+                                        transaction.Rollback();
+                                        throw ex;
+                                    }
+                                }
                             }
                         }
+                        else
+                            MessageBox.Show("Что-то пошло нет так", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DB_Game"].ConnectionString))
+                    {
                         Games = new ObservableCollection<Game>(db.Query<Game>("SELECT * FROM [Game]").ToList());
                         Games.ToList().ForEach(i => i.Studio = Studios.ToList().Find(j => j.Id == i.Game_Studio_id));
                         Games.ToList().ForEach(i => i.Style = Styles.ToList().Find(j => j.Id == i.Game_Style_id));
                         Games.ToList().ForEach(i => i.Mod_Game = Mod_s.ToList().Find(j => j.Id == i.Game_Mod_id));
                         OnPropertyChanged("Games");
                     }
-
-                }
-                else
-                    MessageBox.Show("Что-то пошло нет так", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }*/
-        #endregion
-
-        #region Dell
-        internal void Dell()
-        {
-            if (MessageBox.Show("Удалить игру?", "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
-                return;
-            else
-            {
-                if ( Games.ToList().Exists(i => i.Id == Curent_game.Id))
-                {
-                    using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DB_Game"].ConnectionString))
-                    {
-                        db.Open();
-                        using (var transaction = db.BeginTransaction())
-                        {
-                            try
-                            {
-                                db.Execute(" DELETE FROM [Game] WHERE Id = @Id",
-                                    new { Id = Curent_game.Id }, transaction); 
-                                transaction.Commit();
-                            }
-                            catch (System.Exception ex)
-                            {
-                                transaction.Rollback();
-                                throw ex;
-                            }
-                        }
-                    }
-                }
-                else
-                    MessageBox.Show("Что-то пошло нет так", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-            using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DB_Game"].ConnectionString))
-            {
-                Games = new ObservableCollection<Game>(db.Query<Game>("SELECT * FROM [Game]").ToList());
-                Games.ToList().ForEach(i => i.Studio = Studios.ToList().Find(j => j.Id == i.Game_Studio_id));
-                Games.ToList().ForEach(i => i.Style = Styles.ToList().Find(j => j.Id == i.Game_Style_id));
-                Games.ToList().ForEach(i => i.Mod_Game = Mod_s.ToList().Find(j => j.Id == i.Game_Mod_id));
-                OnPropertyChanged("Games");
+                }));
             }
         }
+                
+
+               
         #endregion
 
         #region ADD
-        internal void ADD()
+        private RelayCommand add;
+        public RelayCommand Add
         {
-            new Window_Add_New().ShowDialog();
-            using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DB_Game"].ConnectionString))
+            get
             {
-                Games = new ObservableCollection<Game>(db.Query<Game>("SELECT * FROM [Game]").ToList());
-                Games.ToList().ForEach(i => i.Studio = Studios.ToList().Find(j => j.Id == i.Game_Studio_id));
-                Games.ToList().ForEach(i => i.Style = Styles.ToList().Find(j => j.Id == i.Game_Style_id));
-                Games.ToList().ForEach(i => i.Mod_Game = Mod_s.ToList().Find(j => j.Id == i.Game_Mod_id));
-                OnPropertyChanged("Games");
+                return add ?? (add = new RelayCommand(act =>
+                {
+                    new Window_Add_New().ShowDialog();
+                    using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DB_Game"].ConnectionString))
+                    {
+                        Games = new ObservableCollection<Game>(db.Query<Game>("SELECT * FROM [Game]").ToList());
+                        Games.ToList().ForEach(i => i.Studio = Studios.ToList().Find(j => j.Id == i.Game_Studio_id));
+                        Games.ToList().ForEach(i => i.Style = Styles.ToList().Find(j => j.Id == i.Game_Style_id));
+                        Games.ToList().ForEach(i => i.Mod_Game = Mod_s.ToList().Find(j => j.Id == i.Game_Mod_id));
+                        OnPropertyChanged("Games");
+                    }
+                }));
             }
         }
+              
         #endregion
 
         #region serch_comand
